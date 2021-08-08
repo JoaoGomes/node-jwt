2d0e69b9bb79643386605103f45543c887b3f109
João Eduardo Gomes <JoaoGomes@github.com>
2021-08-08 09:23:34 -0300

    Adicionando arquivo auth.js

---
+++ auth.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const accessTokenSecret = 'youraccesstokensecret';
+
+const users = [ 
+	{
+		username: 'John',
+		password: 'password123admin',
+		role: 'admin'
+	},
+	{
+		username: 'Anna',
+		password: 'password123member',
+		role: 'member'
+	},
+	{
+		username: 'Ben',
+		password: 'password456member',
+		role: 'member'
+	},
+];
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const user = user.find(u => { return u.username === username && user.password === password});
+
+    if(user) {
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret);
+        res.json({
+            accessToken
+        });
+    } else {
+        res.send('Nome de usuário ou senha incorretos');
+    }
+});
+
+app.listen(3000, () => {
+	console.log('Serviço de autenticação iniciado na porta 3000');
+});
